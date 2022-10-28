82278ef1afd1603a9a28a711ba54876aab374be9
Jagandeep Singh <jagansingh93@gmail.com>
2022-10-27 23:29:04 -0400

    edited dashfile.py

--- dashfile.py
+++ dashfile.py
@@ -4,7 +4,6 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.graph_objs as go
 
 # importing data
