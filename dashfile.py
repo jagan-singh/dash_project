d17fc5d3f785f75673a1d1109cac0f4f00c084c8
Jagandeep Singh <jagansingh93@gmail.com>
2022-10-28 23:43:10 -0400

    edited dashfile

--- dashfile.py
+++ dashfile.py
@@ -3,8 +3,8 @@ import os
 import dash
 import requests
 import io
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import html
+from dash import dcc
 import pandas as pd
 import plotly.graph_objs as go
 
@@ -20,19 +20,26 @@ df_state = pd.read_csv(io.StringIO(download.decode('utf-8')))
 def covid_cases():
     fig = go.Figure([go.Scatter(x = df_us['date'], y = df_us['cases'], line = dict(color = 'firebrick', width = 4))])
     fig.update_layout(title = 'COVID-19 cases in US', xaxis_title = 'Date', yaxis_title = 'Cases' )
-    return fig  
+    return fig
 
 app = dash.Dash(__name__)
 server = app.server
+colors = {
+    'background': '#afbddb',
+    'text': '#0552f7'
+}
+
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children = [
-        dcc.Graph(id = 'line_plot', figure = covid_cases())    
+html.H1(
+        children='Covid Cases in US',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+        dcc.Graph(id = 'line_plot', figure = covid_cases())
     ]
 )
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
-
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
