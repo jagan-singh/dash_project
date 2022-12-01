1f94126d7de2bfdb5d1870db3d877fa487a443ca
Jagandeep Singh <jagansingh93@gmail.com>
2022-12-01 18:15:05 -0500

    added new graphs

--- dashfile.py
+++ dashfile.py
@@ -29,12 +29,18 @@ df_state['new_cases'] = df_state['new_cases'].abs()
 
 def covid_cases(name):
     fig = px.line(x = df_us['date'], y = df_us[name])
-    fig.update_layout(title = 'COVID-19 cases in US')
+    fig.update_layout(title = 'COVID-19 cases in US', paper_bgcolor = '#ffffff', plot_bgcolor = '#ffffff')
     return fig
 
 def states_map():
     fig = px.scatter_geo(df_state, size="new_cases", locationmode = 'USA-states', locations = 'state', scope = 'usa', animation_frame = 'date_updated')
-    fig.update_layout(title = 'States map')
+    fig.update_layout(title = 'States map' , paper_bgcolor = '#ffffff', plot_bgcolor = '#ffffff')
+    return fig
+
+def state_line(state):
+    temp_df = df_state[df_state.state == state]
+    fig = px.line(x = temp_df['date_updated'], y = temp_df['new_cases'])
+    fig.update_layout(title = 'COVID-19 cases per states', paper_bgcolor = '#ffffff', plot_bgcolor = '#ffffff')
     return fig
 
 
@@ -47,23 +53,52 @@ app.layout = html.Div([
 
 home_page = html.Div([
     html.H1('COVID DASHBOARD'),
-    dcc.Dropdown(list(df_us.drop(columns = ['date']).columns), 'cases', id= 'dropdown'),
+    dcc.Dropdown(list(df_us.drop(columns = ['date']).columns), 'cases', id= 'us_dropdown'),
     dcc.Graph(id = 'graph'),
     html.Br(),
-    dcc.Link('State wise new cases', href='/state_wise', id = 'button')
+    dcc.Link('State wise new cases', href='/state_wise', id = 'button'),
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    html.Div(children='Data provided by NY Times'),
+    dcc.Link(href = 'https://github.com/nytimes/covid-19-data' ,target = '_blank')
 ])
 
 @app.callback(
     Output('graph', 'figure'),
-    Input('dropdown', 'value')
+    Input('us_dropdown', 'value')
 )
 def update_output(value):
     return covid_cases(value)
 
 state_wise_layout = html.Div([
-    html.H1('New Covid cases US'),
-    dcc.Graph(id = 'map', figure = states_map())
+    html.H2('New Covid cases US'),
+    dcc.Dropdown(list(df_state['state'].unique()), 'NY', id= 'state_dropdown'),
+    html.Div(children = [
+    dcc.Graph(id = 'graph2', style = {'display': 'inline-block'}),
+    dcc.Graph(id = 'map', figure = states_map() , style = {'display': 'inline-block'})
+    ] ),
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    html.Div(children='Data provided by CDC'),
+    dcc.Link(href = 'https://covid.cdc.gov/covid-data-tracker/#datatracker-home' ,target = '_blank')
 ])
+
+@app.callback(
+    Output('graph2', 'figure'),
+    Input('state_dropdown', 'value')
+)
+def update_output(value):
+    return state_line(value)
 
 @callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
